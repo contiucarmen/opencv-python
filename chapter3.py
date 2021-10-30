abd258c45648b7555a5d67470b97e10a9f8c099f
Carmen <contiucarmen@gmail.com>
2021-10-30 21:37:15 +0300

    first commit

---
+++ chapter3.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread("Resources/photo.jpg")
+print(img.shape)
+
+imgResize = cv2.resize(img,(1000,500))
+print(imgResize.shape)
+
+imgCropped = img[46:119,352:495]
+print(imgCropped.shape)
+
+cv2.imshow("Image",img)
+cv2.imshow("Image Resize",imgResize)
+cv2.imshow("Image Cropped",imgCropped)
+
+cv2.waitKey(0)
